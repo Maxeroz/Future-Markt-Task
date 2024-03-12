29eb470d34484d9d525aee93303784f74c578ec5
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-03-13 03:36:11 +0400

    CSS

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import "normalize.css";
 import { Logo } from "./logo.js";
 import { Menu } from "./menu.js";
 import { Phone } from "./phone.js";
+import WebFont from "webfontloader";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Raleway"],
+      },
+    });
+  }, []);
+
   return (
     <>
       <header className="header">
-        <div
-          className="container header-container"
-          style={{ background: "black" }}
-        >
+        <div className="container header-container">
           <Logo />
           <Menu />
           <Phone />
