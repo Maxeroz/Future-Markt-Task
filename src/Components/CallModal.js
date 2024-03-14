3f355ed73cd46f3975f30eccf0ddfe6b7935bd81
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-03-14 17:07:07 +0400

    480px Mobile

--- src/Components/CallModal.js
+++ src/Components/CallModal.js
@@ -19,63 +19,65 @@ export function CallModal({ active, closeModal }) {
   return (
     // <div className="call-modal-background">
     <div
-      className={
+      className={`${
         active ? "call-modal-background active" : "call-modal-background"
-      }
+      }`}
     >
-      <div className="call-modal">
-        <div className="cross__small">
-          <button onClick={closeModal}>
-            <img src={crossSmall} />
-          </button>
-        </div>
-        <div className="call-modal-title">
-          <h2>
-            Закажите
-            <br></br>
-            обратный звонок
-          </h2>
-        </div>
-        <form className="form-call-modal">
-          <input
-            name="name"
-            placeholder="ИМЯ"
-            className="text-input"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            name="phone"
-            placeholder="ТЕЛЕФОН"
-            className="form-call-phone text-input"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
-          <label className="form-call-div">
+      <div className="call-modal-content">
+        <div className="call-modal-container">
+          <div className="cross__small">
+            <button onClick={closeModal}>
+              <img src={crossSmall} />
+            </button>
+          </div>
+          <div className="call-modal-title">
+            <h2>
+              Закажите
+              <br></br>
+              обратный звонок
+            </h2>
+          </div>
+          <form className="form-call-modal">
             <input
-              type="checkbox"
-              name="myCheckBox"
-              className="form-real-checkbox"
-              checked={isChecked}
-              onChange={() => setIsChecked((is) => !is)}
+              name="name"
+              placeholder="ИМЯ"
+              className="text-input"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
-            <span className="custom-checkbox"></span>
-            <span className="input-text">
-              Согласен на сохранение
-              <br />и обработку персональных данных
-            </span>
-          </label>
-        </form>
-        <Button
-          style={"btn__other__call"}
-          square={"btn__trans-square"}
-          userStyle={{ height: "45px", width: "45px" }}
-          vector={vectorWhite}
-          vectorStyle={"vector__white"}
-          mainClass={"full_btn_call"}
-        >
-          Заказать обратный звонок
-        </Button>
+            <input
+              name="phone"
+              placeholder="ТЕЛЕФОН"
+              className="form-call-phone text-input"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+            <label className="form-call-div">
+              <input
+                type="checkbox"
+                name="myCheckBox"
+                className="form-real-checkbox"
+                checked={isChecked}
+                onChange={() => setIsChecked((is) => !is)}
+              />
+              <span className="custom-checkbox"></span>
+              <span className="input-text">
+                Согласен на сохранение
+                <br />и обработку персональных данных
+              </span>
+            </label>
+          </form>
+          <Button
+            style={"btn__other__call"}
+            square={"btn__trans-square"}
+            userStyle={{ height: "45px", width: "45px" }}
+            vector={vectorWhite}
+            vectorStyle={"vector__white"}
+            mainClass={"full_btn_call"}
+          >
+            Заказать обратный звонок
+          </Button>
+        </div>
       </div>
     </div>
   );
